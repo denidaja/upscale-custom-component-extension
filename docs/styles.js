2ffcd7f09b40d5762424eecfc55c69350f8ba2c6
Daja <ddenidaja@gmail.com>
2021-12-21 17:49:30 +0100

    logging

--- docs/styles.js
+++ docs/styles.js
@@ -441,7 +441,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(true);
 // Module
-___CSS_LOADER_EXPORT___.push([module.i, "", "",{"version":3,"sources":[],"names":[],"mappings":"","sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.i, "body {\n  padding: 0px !important;\n  background-color: transparent !important;\n}\n", "",{"version":3,"sources":["webpack://src/styles.css"],"names":[],"mappings":"AAAA;EACE,uBAAuB;EACvB,wCAAwC;AAC1C","sourcesContent":["body {\n  padding: 0px !important;\n  background-color: transparent !important;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ __webpack_exports__["default"] = (___CSS_LOADER_EXPORT___);
 
